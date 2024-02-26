2c252a78427224cd2af31b8eb3e415d1137d5a04
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-26 21:42:57 +0000

    Implemented Employee class

--- starter 19.03.05/lib/Employee.js
+++ starter 19.03.05/lib/Employee.js
@@ -1,1 +1,29 @@
-// TODO: Write code to define and export the Employee class
+// Employee Class
+class Employee {
+    // Constructor
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    // Methods
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
+}
+
+// Export Employee class
+module.exports = Employee;
