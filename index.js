40660fab04e81089523291a2c7d5360db44a9817
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-26 22:56:39 +0000

    ebugging import statements and pathes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import inquirer from 'inquirer';
 import render from './src/page-template.js';
-import Manager from './lib/Manager';
-import Engineer from './lib/Engineer';
-import Intern from './lib/Intern';
+import Manager from './lib/Manager.js';
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
