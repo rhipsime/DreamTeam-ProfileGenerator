c7ade89b9f5fb66417bb1089864731af5f1d16db
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-26 21:44:27 +0000

    Implemented Engineer class

--- starter 19.03.05/lib/Engineer.js
+++ starter 19.03.05/lib/Engineer.js
@@ -1,1 +1,24 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// Import Employee class
+const Employee = require('./Employee');
+
+// Engineer Class
+class Engineer extends Employee {
+    // Constructor
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+
+    // Method overriding
+    getRole() {
+        return 'Engineer';
+    }
+
+    // Additional method
+    getGithub() {
+        return this.github;
+    }
+}
+
+// Export Engineer class
+module.exports = Engineer;
